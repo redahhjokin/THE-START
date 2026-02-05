a745fba56997923ed970711c9985b0cfec58b048
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-02-05 14:57:51 +0100

    modified:   geeks_institute_bootcamp/week_1/day_2/exercises/exercises_xp_ninja.js

--- geeks_institute_bootcamp/week_1/day_2/exercises/exercises_xp_ninja.js
+++ geeks_institute_bootcamp/week_1/day_2/exercises/exercises_xp_ninja.js
@@ -1,9 +1,58 @@
-// ===== Exercise 1
+// ===== Exercise 1 =====
 
-// ===== Exercise 2
+// ----------- Random Number -------------
 
-// ===== Exercise 3
+let randomNum = prompt("Get a random number between 1 and 100");
+for (let i = 0; i <= randomNum; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 
-// ===== Exercise 4
+// ===== Exercise 2 =====
 
-// ===== Exercise ...
+// ----------- Capitalized letters -------------
+
+function capitalizeLetters(str) {
+  let evenCap = "";
+  let oddCap = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      evenCap += str[i].toUpperCase();
+      oddCap += str[i];
+    } else {
+      evenCap += str[i];
+      oddCap += str[i].toUpperCase();
+    }
+  }
+
+  return [evenCap, oddCap];
+}
+// console.log(capitalizeLetters("abcdef"));
+
+// ===== Exercise 3 =====
+
+// ----------- Is palindrome? -------------
+
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("bob"));
+console.log(isPalindrome("kayak"));
+console.log(isPalindrome("hello"));
+
+// ===== Exercise 4 =====
+
+// ----------- Biggest Number -------------
+
+// ===== Exercise 5 =====
+
+// ----------- Unique Elements -------------
+
+// ===== Exercise 6 =====
+
+// ----------- Calendar -------------
